754a076c12665397c8ce953bb02e7d9acff9b9f1
Yosef Benny Widyokarsono <fybwid@gmail.com>
2020-06-20 10:52:02 +0700

    Update Users routes: - add /users to list all Users - add /:username to show User page - update devise_for

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
+  match '/users',     to: 'users#index',  via: 'get'
+  match '/:username', to: 'users#show',   via: 'get', as: :profile
+  
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
