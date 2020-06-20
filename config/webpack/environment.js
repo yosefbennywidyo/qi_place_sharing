0bb749c5c8edda02c2930e123397d86f08465d13
Yosef Benny Widyokarsono <fybwid@gmail.com>
2020-06-20 19:09:18 +0700

    Webpacker setup: - add app/assets to resolved_paths - update formating

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,14 +2,16 @@ const { environment } = require('@rails/webpacker')
 
 const webpack = require("webpack")
 
-environment.plugins.append("Provide", new webpack.ProvidePlugin({
+environment.plugins.append("Provide",
+  new webpack.ProvidePlugin({
 
-$: 'jquery',
+    $: 'jquery',
 
-jQuery: 'jquery',
+    jQuery: 'jquery',
 
-Popper: ['popper.js', 'default']
+    Popper: ['popper.js', 'default']
 
-}))
+  })
+)
 
 module.exports = environment
