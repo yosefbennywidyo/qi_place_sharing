526e1095eaec7e376b091952aea1c0c33a2e921c
Yosef Benny Widyokarsono <fybwid@gmail.com>
2020-06-21 02:16:29 +0700

    Update place and user models: - add dependent: :destroy

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,4 @@
 class Place < ApplicationRecord
   has_many :place_permissions
-  has_many :users, through: :place_permissions
+  has_many :users, through: :place_permissions, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :place_permissions
-  has_many :places, through: :place_permissions
+  has_many :places, through: :place_permissions, dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
