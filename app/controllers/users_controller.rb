051655c58c664f1c7903d401ac0887f12ab27dce
Yosef Benny Widyokarsono <fybwid@gmail.com>
2020-06-21 18:24:20 +0700

    Update users controller: - add find place_share_with_this_user and   add find public_place_share_with_this_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,15 @@ class UsersController < ApplicationController
   end
 
   def show
-    logger.debug "Check_user_name_params: #{params[:username]}"
+    @public_places = Place.where(public_status: true)
+
     @user = User.find_by_username(params[:username])
+
+    @place_share_with_this_user = PlacePermission.select {|x| x.user_id === "#{@user.id}".to_i}.pluck("place_id")
+
+    @public_place_share_with_this_user = Place.where(id: @place_share_with_this_user).select {|x| x.public_status === true}
+
+    logger.debug "Check_user_name_params: #{params[:username]}"
     logger.debug "Check_user: #{@user}"
     if @user
       render action: :show
